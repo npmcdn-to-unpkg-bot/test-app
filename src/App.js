ea40a02c324f9af36c7464015e0fddce1cd7082e
Chris Heninger <heninger@gmail.com>
2016-08-17 15:52:04 -0700

    eject app, modify production build script to build with node_env set to development

--- src/App.js
+++ src/App.js
@@ -13,7 +13,14 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <AlertOne message="Testing component one (React 15.3)" />
+        <p>
+          <a href="https://github.com/chrisheninger/test-component-one" target="_blank">GitHub Link</a> / <a href="https://npmcdn.com/test-component-one@2.0.0/dist/alert-one.js" target="_blank">npmcdn (dist) Link</a>
+        </p>
+        <br /><br /><br />
         <AlertTwo message="Testing component two (React 15.2)" />
+        <p>
+          <a href="https://github.com/chrisheninger/test-component-two" target="_blank">GitHub Link</a> / <a href="https://npmcdn.com/test-component-two@2.0.0/dist/alert-two.js" target="_blank">npmcdn (dist) Link</a>
+        </p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
